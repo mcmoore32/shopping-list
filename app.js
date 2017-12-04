497ea2aa9cce39ef29a0472d9c74bdd3fdd4384e
Michael Moore <mcmoore32@gmail.com>
2017-12-04 09:16:11 -0500

    Add items working - getting form input and creating the shopping list

---
+++ app.js
@@ -0,0 +1,29 @@
+/*
+ALgorithm
+
+Capture the form input and store the value in array
+
+Creat list items from the array
+
+*/
+
+function addListItems(){
+  var itemArray = [];
+  var listItem = '';
+  $('#js-shopping-list-form').submit( function(event){
+    event.preventDefault();
+    var itemEntered = $('#shopping-list-entry').val();
+    itemArray.push(itemEntered);
+    var listItem = '';
+    for(i=0; i<itemArray.length; i++) {
+      listItem = listItem + '<li><span class = "shopping-item">' + itemArray[i] + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
+      $('.shopping-list').html(listItem);
+    }
+  });
+  return itemArray;
+};
+
+var out = addListItems();
+console.log(out);
+
+
