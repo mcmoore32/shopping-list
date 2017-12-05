7abe88ba9197a81b1b19874ca5ba0e9bc2dba55f
Michael Moore <mcmoore32@gmail.com>
2017-12-05 17:53:28 -0500

    Delete Items function is working

--- app.js
+++ app.js
@@ -5,18 +5,40 @@ Capture the form input and store the value in array
 
 Creat list items from the array
 
+$('this.shopping item').html();
+
+<span class="shopping-item">milk</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div>
+
 */
 
-function makeList(arrayVariable, listVariable){
-  for(var i=0; i<arrayVariable.length; i++) {
-    listVariable = listVariable + '<li><span class = "shopping-item">' + arrayVariable[i] + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
-    $('.shopping-list').html(listVariable);
-  };
+var itemArray = [];
+var listItem = "";
+var deletedItem = "";
+
+function makeList(){
+  listItem = "";
+  if (itemArray.length !== 0) {
+    for(var i=0; i<itemArray.length; i++) {
+      listItem = listItem + '<li><span class = "shopping-item">' + itemArray[i] + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
+    }
+  } else {listItem = "";}
+    $('.shopping-list').html(listItem);
+    deleteListItems();
+};
+
+function checkForDeleted(item) {
+  return item !== deletedItem;
 };
 
 function deleteListItems(){
   $('.shopping-item-delete').click(function(){
     console.log("Delete button pushed");
+    deletedItem = $(this.closest('li')).html();
+    deletedItem = deletedItem.slice(28, -220);
+    itemArray = itemArray.filter(checkForDeleted);
+    makeList();
+    console.log(deletedItem);
+    console.log(itemArray);
   });
 };
 
@@ -27,23 +49,18 @@ function checkListItems(){
 };
 
 function addListItems(){
-  var itemArray = [];
-  var listItem = "";
   $('#js-shopping-list-form').submit(function(){
     event.preventDefault();
     var itemEntered = $('#shopping-list-entry').val();
     itemArray.push(itemEntered);
     $('#js-shopping-list-form')[0].reset();
-    listItem = '';
-    makeList(itemArray, listItem);
-    deleteListItems();
+    makeList();
+    //    deleteListItems();
     checkListItems();
   });
 };
 
 addListItems();
-//deleteListItems();
-//checkListItems();
 
 
 
