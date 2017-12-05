3d65249196c81ea5fe19e0a7ac228e29a7002500
Michael Moore <mcmoore32@gmail.com>
2017-12-05 10:20:57 -0500

    Event listeners working

--- app.js
+++ app.js
@@ -7,22 +7,47 @@ Creat list items from the array
 
 */
 
+function makeList(arrayVariable, listVariable){
+  for(var i=0; i<arrayVariable.length; i++) {
+    listVariable = listVariable + '<li><span class = "shopping-item">' + arrayVariable[i] + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
+    $('.shopping-list').html(listVariable);
+  };
+};
+
+function deleteListItems(){
+  $('.shopping-item-delete').click(function(){
+    console.log("Delete button pushed");
+  });
+};
+
+function checkListItems(){
+  $('.shopping-item-toggle').click(function(){
+    console.log("Check button pushed");
+  });
+};
+
 function addListItems(){
   var itemArray = [];
-  var listItem = '';
-  $('#js-shopping-list-form').submit( function(event){
+  var listItem = "";
+  $('#js-shopping-list-form').submit(function(){
     event.preventDefault();
     var itemEntered = $('#shopping-list-entry').val();
     itemArray.push(itemEntered);
-    var listItem = '';
-    for(i=0; i<itemArray.length; i++) {
-      listItem = listItem + '<li><span class = "shopping-item">' + itemArray[i] + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
-      $('.shopping-list').html(listItem);
-    }
+    $('#js-shopping-list-form')[0].reset();
+    listItem = '';
+    makeList(itemArray, listItem);
+    deleteListItems();
+    checkListItems();
   });
 };
 
 addListItems();
+//deleteListItems();
+//checkListItems();
+
+
+
+
 
 
 
